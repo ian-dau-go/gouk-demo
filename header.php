23cdbf094ee85e5a4064f9f1c69d3cc0dda5aeab
ian-dau-go <ian.dau.goldenowl@gmail.com>
2023-08-14 14:59:38 +0700

    merge code

--- header.php
+++ header.php
@@ -6,8 +6,10 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php if (isset($args['title'])) echo $args['title'];
-            else echo 'Unknow page' ?></title>
+    <title>
+        <?php if (isset($args['title'])) echo $args['title'];
+        else echo 'Unknow page' ?>
+    </title>
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/index.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/cdnjs.cloudflare.com_ajax_libs_OwlCarousel2_2.3.4_assets_owl.carousel.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/cdnjs.cloudflare.com_ajax_libs_OwlCarousel2_2.3.4_assets_owl.theme.default.min.css" type="text/css" media="screen" />
@@ -15,8 +17,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-get_header('',["title" => 'Home Page']); 
+get_header('', ["title" => 'Home Page']);
 
-get_template_part('template-parts/home/welcome');  
-get_template_part('template-parts/home/partners-marquee');  
-get_template_part('template-parts/home/partners');  
-get_template_part('template-parts/home/why-choose-golden-owl');   
-get_template_part('template-parts/home/vision');  
-get_template_part('template-parts/home/our-team'); 
+get_template_part('template-parts/home/banner');
+get_template_part('template-parts/home/partners-marquee');
+get_template_part('template-parts/home/partners');
+get_template_part('template-parts/home/why-choose-golden-owl');
+get_template_part('template-parts/home/vision');
+get_template_part('template-parts/home/our-team');
 get_template_part('template-parts/home/our-services');
 get_template_part('template-parts/home/our-archievements');
 get_template_part('template-parts/home/our-history-of-development');
