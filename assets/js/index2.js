2980a1ad84acc24f1e025625cfde8a1469058b8f
Hoàng Trương <oscar.truong.goldenowl@gmail.com>
2023-08-16 17:22:27 +0700

    update responsive homepage slide partners in mobile

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -1,58 +1,57 @@
-
 (function () {
-    init();
-    function init() {
-        wheelHandler();
-        // mousemoveHandler();
+  init();
+  function init() {
+    wheelHandler();
+    // mousemoveHandler();
 
-        $(document).ready(function () {
-            $('.owl-carousel').owlCarousel({
-                // autoplay: true,
-                // /*center: true,
-                // loop: true,
-                // infinite:true,
-                nav: false,
-                // rtl: true,
-                dots: true,
-                items: 1,
-                autoplayTimeout: 4000,
-                autoplayHoverPause: true,
-            });
-          
-          });
-          
-    }
+    $(document).ready(function () {
+      $(".owl-carousel").owlCarousel({
+        autoplay: true,
+        center: true,
+        // margin: 10,
+        loop: true,
+        infinite: true,
+        nav: false,
+        // rtl: true,
+        dots: true,
+        items: 1,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+      });
+    });
+  }
 
-    function wheelHandler(evt) {
-        const scrollContainer = document.querySelector(".frame34786");
-        scrollContainer && scrollContainer.addEventListener("wheel", (evt) => {
-            evt.preventDefault();
-            scrollContainer.scrollLeft += evt.deltaY;
-        });
-    }
+  function wheelHandler(evt) {
+    const scrollContainer = document.querySelector(".frame34786");
+    scrollContainer &&
+      scrollContainer.addEventListener("wheel", (evt) => {
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+      });
+  }
 
-    function mousemoveHandler() {
-        let timeline = document.querySelector('.timeline');
+  function mousemoveHandler() {
+    let timeline = document.querySelector(".timeline");
 
-        timeline.addEventListener('mousemove', function (e) {
-            let circle = document.getElementById('timelineCircle');
-            var offsetX = e.offsetX;
-            var offsetY = e.offsetY;
-            var x = e.clientX; 
-            var y = e.clientY; 
+    timeline.addEventListener("mousemove", function (e) {
+      let circle = document.getElementById("timelineCircle");
+      var offsetX = e.offsetX;
+      var offsetY = e.offsetY;
+      var x = e.clientX;
+      var y = e.clientY;
 
-            // var element = e.target;
-            var rect = timeline.getBoundingClientRect(); 
-			var offsetX = x - rect.left; 
-			var offsetY = y - rect.top; 
-		
-            // while (element !== timeline) {
-                // offsetY += element.offsetTop;
-                // element = element.parentNode;
-            // }
+      // var element = e.target;
+      var rect = timeline.getBoundingClientRect();
+      var offsetX = x - rect.left;
+      var offsetY = y - rect.top;
 
-            circle.style.left = offsetX + 'px';
-            circle.style.top = offsetY + 'px';
-        });
-    }
+      // while (element !== timeline) {
+      // offsetY += element.offsetTop;
+      // element = element.parentNode;
+      // }
+
+      circle.style.left = offsetX + "px";
+      circle.style.top = offsetY + "px";
+    });
+  }
 })();
